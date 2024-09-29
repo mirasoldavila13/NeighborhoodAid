41c9cd6d459c4d060ce597b314414c27f89dfd09
Mirasol D. <mirasoldavila13@aim.com>
2024-09-29 10:19:31 -0700

    Add all sections to App component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,23 @@
-import Header from "./components/Header";
+import Nav from "./components/Nav";
 import Hero from "./components/Hero";
-import './utils/script';
-const App = () => {
+import Features from "./components/Features";
+import FAQ from "./components/FAQ";
+import Testimonials from "./components/Testimonials";
+import Newsletter from "./components/Newsletter";
+import Footer from "./components/Footer";
+
+function App() {
   return (
-    <div>
-      <Header />
+    <>
+      <Nav />
       <Hero />
-    </div>
+      <Features />
+      <FAQ />
+      <Testimonials />
+      <Newsletter />
+      <Footer />
+    </>
   );
-};
+}
 
 export default App;
