c51941127687e21f188ada54281a4807fb00896d
Mirasol D. <mirasoldavila13@aim.com>
2024-09-29 13:16:10 -0700

    feat: Create signup form with email/password input, styled Sign Up button, added routing, and implemented back navigation to home.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./components/LandingPage";
 import Login from "./pages/Login";
+import SignUp from "./pages/Signup";
 
 function App() {
   return (
@@ -8,7 +9,8 @@ function App() {
       <Routes>
         {/* Route for the landing page */}
         <Route path="/" element={<LandingPage />} />
-
+        {/* Route for the sign up page */}
+        <Route path="/signup" element={<SignUp />} />
         {/* Route for the login page */}
         <Route path="/login" element={<Login />} />
       </Routes>
--- client/src/components/Nav.tsx
+++ client/src/components/Nav.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Link } from "react-router-dom"; // Import Link from react-router-dom
+import { Link } from "react-router-dom";
 import logo from "../assets/neighborhood_logo.svg";
 
 const Nav = () => {
