9e858d0d1d0cb5ff6be7314b8d15933a92c8f2ec
Mirasol D. <mirasoldavila13@aim.com>
2024-09-29 14:09:58 -0700

    feat: Updated background color of Testimonials and Newsletter sections to match Pricing page for consistent design.

--- client/src/components/Newsletter.tsx
+++ client/src/components/Newsletter.tsx
@@ -1,20 +1,23 @@
 const Newsletter = () => (
-  <section id="early-access" className="relative px-6 md:px-0">
-    <div className="relative max-w-4xl mx-auto p-10 space-y-6 text-center rounded-lg bg-gray-200 shadow-lg md:px-16">
-      <h5 className="text-2xl font-bold">Get early access today</h5>
-      <p className="text-sm">
+  <section
+    id="early-access"
+    className="relative px-6 md:px-0 py-12 bg-gray-800"
+  >
+    <div className="relative max-w-4xl mx-auto p-8 space-y-4 text-center rounded-lg bg-gray-700 shadow-lg md:px-12">
+      <h5 className="text-2xl font-bold text-white">Get early access today</h5>
+      <p className="text-sm text-gray-300">
         It only takes a minute to sign up and our free starter tier is extremely
         generous.
       </p>
-      <div className="flex flex-col items-start space-y-6 md:flex-row md:space-x-6">
+      <div className="flex flex-col items-start space-y-4 md:flex-row md:space-x-4">
         <div className="w-full md:flex-1">
           <input
             type="text"
-            className="w-full px-10 py-3 rounded-full border focus:outline-none focus:ring-2 focus:ring-purpleLight"
+            className="w-full px-8 py-3 rounded-full border focus:outline-none focus:ring-2 focus:ring-purpleLight"
             placeholder="email@example.com"
           />
         </div>
-        <button className="w-full p-3 rounded-full bg-purpleLight text-white md:w-56 hover:bg-darkViolet transition">
+        <button className="w-full p-3 rounded-full bg-purpleLight text-white md:w-48 hover:bg-darkViolet transition">
           Get Started For Free
         </button>
       </div>
