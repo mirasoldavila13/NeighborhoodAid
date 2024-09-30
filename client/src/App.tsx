324bbbe7c3106ecaa2bd827c9dcd3371c6fc2a2c
Mirasol D. <mirasoldavila13@aim.com>
2024-09-29 19:32:32 -0700

    updated

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./components/LandingPage";
 import Features from "./pages/Features";
+import Pricing from "./components/Pricing";
+import TestimonialsPage from "./pages/Testimonials";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
-import Pricing from "./components/Pricing";
+import Dashboard from "./pages/Dashboard";
+
 function App() {
   return (
     <Router>
@@ -12,12 +15,15 @@ function App() {
         <Route path="/" element={<LandingPage />} />
         {/* Route for the features page */}
         <Route path="/features" element={<Features />} />
+        {/* Route for Pricing page*/}
+        <Route path="/pricing" element={<Pricing />} />
+        {/* Route for Testimonials Page */}
+        <Route path="/testimonials" element={<TestimonialsPage />} />
         {/* Route for the sign up page */}
         <Route path="/signup" element={<SignUp />} />
         {/* Route for the login page */}
         <Route path="/login" element={<Login />} />
-        {/* Route for Pricing page*/}
-        <Route path="/pricing" element={<Pricing />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
   );
