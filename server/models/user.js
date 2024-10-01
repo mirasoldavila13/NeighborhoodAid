b063ab1ce7f6f231181fa5d30bb65949d6cdf3da
Justin Kao <justinkao010@gmail.com>
2024-10-01 11:37:46 -0700

    removed password hashing from db

--- server/models/user.js
+++ server/models/user.js
@@ -1,13 +1,6 @@
 import { DataTypes, Model } from "sequelize";
-import bcrypt from "bcrypt";
 
-export class User extends Model {
-  // Hash password before saving user
-  async setPassword(password) {
-    const saltRounds = 10;
-    this.password = await bcrypt.hash(password, saltRounds);
-  }
-}
+export class User extends Model {}
 
 export function UserTable(sequelize) {
   User.init(
@@ -35,16 +28,7 @@ export function UserTable(sequelize) {
     {
       tableName: "users",
       sequelize,
-      // utliize hash on passwords before saving to db
-      hooks: {
-        beforeCreate: async (user) => {
-          await user.setPassword(user.password);
-        },
-        beforeUpdate: async (user) => {
-          await user.setPassword(user.password);
-        },
-      },
-    },
+    }
   );
   return User;
 }
