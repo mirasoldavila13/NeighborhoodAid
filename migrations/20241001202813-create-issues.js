c1ec68d3835cb324ab23763bbcb32dbca0577428
Justin Kao <justinkao010@gmail.com>
2024-10-01 13:46:13 -0700

    added migration file for issues and adjusted user migration for model

--- migrations/20241001202813-create-issues.js
+++ migrations/20241001202813-create-issues.js
@@ -1,22 +1,27 @@
-"use strict";
+'use strict';
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable("Users", {
       id: {
-        allowNull: false,
+        type: Sequelize.INTEGER,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER,
       },
-      name: {
+      username: {
         type: Sequelize.STRING,
+        unique: true,
+        allowNull: false,
       },
       email: {
         type: Sequelize.STRING,
+        unique: true,
+        allowNull: false,
       },
       password: {
         type: Sequelize.STRING,
+        allowNull: false,
       },
       createdAt: {
         allowNull: false,
@@ -28,7 +33,8 @@ module.exports = {
       },
     });
   },
+
   async down(queryInterface, Sequelize) {
     await queryInterface.dropTable("Users");
-  },
+  }
 };
