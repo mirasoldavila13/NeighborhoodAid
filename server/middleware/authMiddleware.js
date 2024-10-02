f8a0ccba7f51dd680de10ffaf289c27d5535b262
Justin Kao <justinkao010@gmail.com>
2024-10-01 19:53:05 -0700

    waiting for issue form

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,22 +1,22 @@
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 
 export const authenticateToken = (req, res, next) => {
-    const authHeader = req.headers.authorization;
-    console.log(authHeader);
+  const authHeader = req.headers.authorization;
+  console.log(authHeader);
 
-    if (authHeader) {
-        const token = authHeader.split(' ')[1];
-        const secretKey = process.env.JWT_SECRET;
+  if (authHeader) {
+    const token = authHeader.split(" ")[1];
+    const secretKey = process.env.JWT_SECRET;
 
-        jwt.verify(token, secretKey, (err, user) => {
-            if (err) {
-                return res.sendStatus(403); // Send forbidden status if token invalid
-            }
+    jwt.verify(token, secretKey, (err, user) => {
+      if (err) {
+        return res.sendStatus(403); // Send forbidden status if token invalid
+      }
 
-            req.user = user; // Attaches user info to object
-            return next();
-        });
-    } else {
-        res.sendStatus(401); // Send unauthorized status if no header
-    }
+      req.user = user; // Attaches user info to object
+      return next();
+    });
+  } else {
+    res.sendStatus(401); // Send unauthorized status if no header
+  }
 };
