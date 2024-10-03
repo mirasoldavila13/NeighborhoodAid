3d6e6ec5e19ef8d059bc237e92cfb393ed8da5cf
Mirasol D. <mirasoldavila13@aim.com>
2024-10-02 22:48:12 -0700

    Add JWT authentication middle ware in auth.js to verify tokens and authenticate user requests

---
+++ server/middleware/authMiddleware.js
@@ -0,0 +1,18 @@
+// Handles JWT or session authentication
+import jwt from "jsonwebtoken";
+
+export default (req, res, next) => {
+  const token = req.headers["authorization"];
+
+  if (!token) {
+    return res.status(403).json({ message: "No token provided" });
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(500).json({ message: "Failed to authenticate token" });
+    }
+    req.userId = decoded.id;
+    next();
+  });
+};
