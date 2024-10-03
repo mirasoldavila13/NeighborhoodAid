7f805ac84be4077d84724a32e1a2cb642f72e41d
Mirasol D. <mirasoldavila13@aim.com>
2024-10-03 13:55:33 -0700

    updated

--- client/src/pages/Testimonials.tsx
+++ client/src/pages/Testimonials.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Link } from "react-router-dom";
 import Nav from "../components/Nav"; // Ensure Nav component path is correct
 import Footer from "../components/Footer"; // Ensure Footer component path is correct
