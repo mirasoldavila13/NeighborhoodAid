68930b80bd718d5a8aea53007a42a4c4e1a4fce6
Mirasol D. <mirasoldavila13@aim.com>
2024-10-02 23:36:58 -0700

    Set up RESTful API routes for feeds: GET to fetch all feeds, POST to create new feed

--- server/routes/feed.js
+++ server/routes/feed.js
@@ -1,13 +1,13 @@
-import express from 'express';
-import feedController from '../controllers/feedController.js';
-import authMiddleware from '../middleware/auth.js';
+import express from "express";
+import feedController from "../controllers/feedController.js";
+import authMiddleware from "../middleware/auth.js";
 
 const router = express.Router();
 
 // Route to fetch all feeds (public)
-router.get('/', feedController.getAllFeeds);
+router.get("/", feedController.getAllFeeds);
 
 // Route to create a feed (protected)
-router.post('/', authMiddleware, feedController.createFeed);
+router.post("/", authMiddleware, feedController.createFeed);
 
 export default router;
