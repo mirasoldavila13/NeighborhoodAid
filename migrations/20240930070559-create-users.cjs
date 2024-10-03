4e40b7a6b14a16e8ae5d34e80028f86d0ed45e78
Mirasol D. <mirasoldavila13@aim.com>
2024-10-03 13:46:09 -0700

    Added migration for users table with fields for id, name, email, password

---
+++ migrations/20240930070559-create-users.cjs
@@ -0,0 +1,37 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Users", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        type: Sequelize.STRING,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false, // Email is required
+        unique: true, // Email must be unique
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false, // Password is required
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("Users");
+  },
+};
