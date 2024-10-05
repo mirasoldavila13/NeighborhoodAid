f409902cf98444a920637553b44fd3a5d1d79029
Justin Kao <justinkao010@gmail.com>
2024-10-05 10:35:49 -0700

    removed index since protected routes are within their respective routes

--- server/routes/index.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import authRoutes from './authRoutes.js';
-import issueRoutes from './issueRoutes.js';
-import  authMiddleware from '../middleware/authMiddleware.js';
-
-const router = Router();
-router.use('/api/auth', authRoutes);
-router.use('/api/issues', authMiddleware, issueRoutes); // Need auth token to access files in api
-export default router;
