9cacd8920aa37e9de7ce6fa6d1327f82f3056bcc
Mirasol D. <mirasoldavila13@aim.com>
2024-10-09 23:57:54 -0700

    chore(migrations): update create-feed-table migration to check for table existence before creation in v2.0

--- migrations/20241003075958-create-feed-table.cjs
+++ migrations/20241003075958-create-feed-table.cjs
@@ -2,36 +2,51 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    const { DataTypes } = Sequelize; // Add this line to define DataTypes
-    await queryInterface.createTable("Feeds", {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: DataTypes.INTEGER, // Use DataTypes here
-      },
-      content: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      userId: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-      },
-      likes: {
-        type: DataTypes.INTEGER,
-        defaultValue: 0,
-      },
-      createdAt: {
-        allowNull: false,
-        type: DataTypes.DATE,
-      },
-      updatedAt: {
-        allowNull: false,
-        type: DataTypes.DATE,
-      },
-    });
+    const { DataTypes } = Sequelize;
+
+    // Check if the table already exists
+    const tableExists = await queryInterface.sequelize.query(`
+      SELECT EXISTS (
+        SELECT FROM information_schema.tables 
+        WHERE table_name = 'Feeds'
+      );
+    `);
+
+    // If the table does not exist, create it
+    if (!tableExists[0][0].exists) {
+      await queryInterface.createTable("Feeds", {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: DataTypes.INTEGER,
+        },
+        content: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+        userId: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+        },
+        likes: {
+          type: DataTypes.INTEGER,
+          defaultValue: 0,
+        },
+        createdAt: {
+          allowNull: false,
+          type: DataTypes.DATE,
+          defaultValue: Sequelize.fn('now'),
+        },
+        updatedAt: {
+          allowNull: false,
+          type: DataTypes.DATE,
+          defaultValue: Sequelize.fn('now'),
+        },
+      });
+    }
   },
+
   async down(queryInterface, Sequelize) {
     await queryInterface.dropTable("Feeds");
   },
