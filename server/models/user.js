cdb501ea2ae377d9e39c332b5621a4f838081f86
Mirasol D. <mirasoldavila13@aim.com>
2024-10-10 14:28:37 -0700

    Enhance User model with timestamps and password validation; improve association documentation.

--- server/models/user.js
+++ server/models/user.js
@@ -1,35 +1,47 @@
 "use strict";
-import { Model } from "sequelize";
+import { Model, DataTypes } from "sequelize";
 
-export default (sequelize, DataTypes) => {
+const User = (sequelize) => {
   class User extends Model {
     static associate(models) {
-      // define association here
+      // Define association with Feed model
       User.hasMany(models.Feed, { foreignKey: "userId" });
     }
   }
 
   User.init(
     {
-      name: DataTypes.STRING,
+      name: {
+        type: DataTypes.STRING,
+        allowNull: true, // Allow null if necessary
+      },
       email: {
         type: DataTypes.STRING,
         allowNull: false,
         unique: true,
         validate: {
-          isEmail: true,
+          isEmail: true, // Validate email format
         },
       },
       password: {
         type: DataTypes.STRING,
         allowNull: false,
+        validate: {
+          len: {
+            args: [6, 100], // Minimum length for password
+            msg: "Password must be between 6 and 100 characters long",
+          },
+        },
       },
     },
     {
       sequelize,
       modelName: "User",
+      timestamps: true, // Enable timestamps
     },
   );
 
   return User;
 };
+
+export default User; 
