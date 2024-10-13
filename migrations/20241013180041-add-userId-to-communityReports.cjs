52705bc9ad2360d5276ab995133da187e742b1e4
Mirasol D. <mirasoldavila13@users.noreply.github.com>
2024-10-13 12:25:17 -0700

    migrate: Add userId column to CommunityReports table for v2.0

---
+++ migrations/20241013180041-add-userId-to-communityReports.cjs
@@ -0,0 +1,18 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.addColumn('CommunityReports', 'userId', {
+      type: Sequelize.INTEGER,
+      allowNull: true, 
+      references: {
+        model: 'Users',
+        key: 'id',
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.removeColumn('CommunityReports', 'userId');
+  },
+};
