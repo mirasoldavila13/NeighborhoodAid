7473b318f5c9bd3f58f47fad66b80abae6e31b49
Mirasol D. <mirasoldavila13@aim.com>
2024-10-12 17:56:42 -0700

    Add status column to ReportAuthorities table for v2.0 Signed-off-by: Mirasol Davila <mirasoldavila13@gmail.com>

---
+++ migrations/20241013003103-add-status-to-report-authorities.cjs
@@ -0,0 +1,18 @@
+'use strict';
+
+import { DataTypes } from 'sequelize';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.addColumn('ReportAuthorities', 'status', {
+      type: Sequelize.STRING,
+      allowNull: false,
+      defaultValue: 'Open', // Set default status to 'Open'
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.removeColumn('ReportAuthorities', 'status');
+  }
+};
